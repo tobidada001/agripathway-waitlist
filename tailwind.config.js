504dfd0ddeb251b6665e8c7700577a772adc37ac
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2025-02-27 20:48:44 +0100

    Created folder for waitlist and setup tailwind correctly

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        './*.{html,js}', // Adjust the path according to your project structure
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
